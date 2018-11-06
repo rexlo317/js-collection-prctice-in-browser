840a74fcb3bec1a8c66338d2673e966f4feaf688
Rex Lo <LORE2@OOCL.COM>
2018-11-06 15:33:31 +0800

    Practice-3

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var result = [];
+	var k = 0;
+	var arraylengthA = collectionA.length;
+	var arrayB = objectB.value;
+	var arraylengthB = arrayB.length;
+	for (var i=0; i<arraylengthA; i++)
+		for (var j=0; j<arraylengthB; j++)
+			if (collectionA[i] == arrayB[j])
+			{
+				result[k] = collectionA[i]
+				k++;
+			}
+	console.log(result);
+	return result;
 }

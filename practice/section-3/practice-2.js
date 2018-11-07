65fff7941a4e7bb66c64f29dabd2533a66e6ce54
Rex Lo <LORE2@OOCL.COM>
2018-11-07 16:03:26 +0800

    section 2 practice-2 modification

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -4,21 +4,15 @@ function createUpdatedCollection(collectionA, objectB) {
 	var arrayAlength = collectionA.length;
 	var arrayB = objectB.value;
 	var arrayBlength = arrayB.length;
-	let count = 0;
-	for(let j=0; j<arrayBlength; j++)
+	let charCount = 0;
+	for(let j=0; j<arrayBlength; j++){
 		for (let i=0; i<arrayAlength; i++)
 		{		
 			if (collectionA[i].key === arrayB[j]){
-				if (count === 0){
-					collectionA[i].count -= 1;
-					count++;
-				}
-				else{
-					collectionA[i].count -= count;
-					count++;
-				}
+				collectionA[i].count -= Math.floor(collectionA[i].count/3)
 			}
 		}
+	}
 	console.log(collectionA);
 	return collectionA;
 }

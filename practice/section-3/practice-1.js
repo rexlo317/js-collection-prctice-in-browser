9bddedb0739229241a84efa49a97c6cc6a883f77
Rex Lo <LORE2@OOCL.COM>
2018-11-07 15:45:02 +0800

    section 3 practice-1

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var arrayAlength = collectionA.length;
+	var arrayB = objectB.value;
+	var arrayBlength = arrayB.length;
+	var result = [];
+	for (let i=0; i<arrayAlength; i++)
+		for(let j=0; j<arrayBlength; j++)
+		{		
+			if (collectionA[i].key === arrayB[j]){
+				collectionA[i].count -= 1;
+			}
+		}
+	console.log(collectionA);
+	return collectionA;
 }

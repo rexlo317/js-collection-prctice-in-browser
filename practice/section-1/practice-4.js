97e46daa6c41879321fd046d49b68bf985f3c7e6
Rex Lo <LORE2@OOCL.COM>
2018-11-21 11:52:15 +0800

    refactor section 1 practice-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,19 +1,5 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-	var result = [];
-	var k = 0;
-	var arraylengthA = collectionA.length;
-	var arrayB = objectB.value;
-	var arraylengthB = arrayB.length;
-	for (var i=0; i<arraylengthA; i++){
-		for (var j=0; j<arraylengthB; j++)
-			if (Object.values(collectionA[i]) == arrayB[j])
-			{
-				result[k] = Object.values(collectionA[i])[0];
-				k++;
-			}
-	}
-	console.log(result);
-	return result;
+function collectSameElements(collectionA, collectionB) {
+	return collectionA.filter( a => collectionB.value.includes(a.key)).map( aWithKey => aWithKey.key);
 }

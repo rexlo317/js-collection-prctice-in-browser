57c2c28012cb7b241db4b34aa2ca818988f00909
Rex Lo <LORE2@OOCL.COM>
2018-11-21 12:14:45 +0800

    tried to create myFilter

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
+	collectionA.myFilter = myFilter;
+	return collectionA.myFilter((a) => collectionB.includes(a));
+}
+
+function myFilter(functionPara){
+	let filteredCollection=[];
+	for (let aCount=0; aCount<this.length; aCount++)
+	{
+		let pointer = 0;
+		if (functionPara(this[aCount])){
+			filteredCollection.push(this[aCount]);
+		}
+	}
+	return filteredCollection;
+}
+/*
+function collectSameElements(collectionA, collectionB) {
 	return collectionA.filter((a) => collectionB.includes(a));
 }
+*/
